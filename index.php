ec2e6d48db636c11d68b341a53343825ab58a3af
Storm Dragon <stormdragon2976@gmail.com>
2017-04-10 21:37:52 -0400

    More work on index.php.

--- index.php
+++ index.php
@@ -1,5 +1,26 @@
 <?php
+ob_start();
+// Get variables
+if (!isset($_GET['api']))
+{
+$api = $_GET['api'];
+}
+if (!isset($_GET['type']))
+{
+$type = $_GET['type'];
+}
+else
+{
+$type = "default";
+}
+// this makes for an ever expanding joke catalog:
+switch ("$type")
+{
+default:
 require_once("scripts/jokes.php");
+}
 // Return a random joke from the list:
 echo $jokeList[array_rand($jokeList)];
+
+ob_end_flush();
 ?>
