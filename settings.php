b1a1a34728e3bb10123c48a613d554654625a57e
Storm Dragon <stormdragon2976@gmail.com>
2017-04-13 07:35:50 -0400

    Left off a $.

--- settings.php
+++ settings.php
@@ -2,7 +2,7 @@
 // Email address you want new jokes sent to, leave blank to disable this feature:
 $email = "";
 // Your GNU Social url
-gs = "";
+$gs = "";
 // Title of your site:
 $title = "Jokes API Site";
 // The type of joke you want to be the default. Check scripts directory for file names to use here:
