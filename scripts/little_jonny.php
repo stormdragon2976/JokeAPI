d0620de2e46b4f7fc084a361dcd82bc05bb9e319
Storm Dragon <stormdragon2976@gmail.com>
2017-04-13 21:09:38 -0400

    Little Johnny jokes started.

---
+++ scripts/little_jonny.php
@@ -0,0 +1,97 @@
+$jokeList = array(
+"Little April was not the best student in Sunday school.
+Usually she slept through the class.
+One day the teacher called on her while she was napping, \"Tell me, April, who created the universe?\"
+When April didn't stir, little Johnny, a boy seated in the chair behind her, took a pin and jabbed her in the rear. 
+\"GOD ALMIGHTY!\" shouted April and the teacher said, \"Very good\" and April fell back asleep.
+A while later the teacher asked April, \"Who is our Lord and Saviour,\" But, April didn't even stir from her slumber.
+Once again, Johnny came to the rescue and stuck her again. 
+\"JESUS CHRIST!\" shouted April and the teacher said, \"very good,\" and April fell back to sleep.
+Then the teacher asked April a third question. 
+\"What did Eve say to Adam after she had her twenty-third child?\" 
+And again, Johnny jabbed her with the pin. 
+This time April jumped up and shouted, \"IF YOU STICK THAT FuckinG THING IN ME ONE MORE TIME, I'LL BREAK IT IN HALF AND STICK IT UP YOUR AsS!\"
+The Teacher fainted.",
+"Little Johnny's teacher is warned at the beginning of the school year not to ever make a bet with Johnny unless she is absolutely sure she will win it.
+One day in class, Johnny raises his hand and says \"teacher, I'll bet you $50 I can guess what color your underwear is.\"
+She replies, \"okay, meet me after class and we'll settle it.\" But beforeclass ends, she goes to the restroom and removes her panties.
+After class is over and the studentsclear out, Johnny makes his guess.
+\"Blue.\"
+\"Nope. You got it wrong,\" she says as she lifts her skirt to reveal she isn't wearing any underwear.
+\"Well come with me out to my dads car, he's waiting for me, and I'll get you the money.\" She follows him out.
+When they get to the car she informs his dad that he got the bet wrong and that she showed Johnny that she wasn't wearing any underwear.
+His dad exclaims: \"That mother fucker! He bet me $100 this morning that he'd see your pussy before the end of the day!\"",
+"Little Johnny was sitting in class doing math problems when his teacher picked him to answer a question, \"Johnny, if there were five birds sitting on a fence and you shot one with your gun, how many would be left?\"
+\"None,\" replied Johnny, \"cause the rest would fly away.\"
+\"Well, the answer is four,\" said the teacher, \"but I like the way you're thinking.\" 
+Little Johnny says, \"I have a question for you. If there were three women eating ice cream cones in a shop, one was licking her cone, the second was biting her cone and the third was sucking her cone, which one is married?\" 
+\"Well,\" said the teacher nervously, \"I guess the one sucking the cone.\" 
+\"No,\" said Little Johnny, \"the one with the wedding ring on her finger, but I like the way you're thinking.\"",
+"A new teacher was trying to make use of her psychology courses.
+She started her class by saying, \"Everyone who thinks they're stupid, stand up!\"
+After a few seconds, Little Johnny stood up. 
+The teacher said, \"Do you think you're stupid, Little Johnny?\"
+\"No, ma'am, but I hate to see you standing there all by yourself!\"",
+"A teacher was working with a group of children, trying to broaden their horizons through sensory perception.
+She brought in a variety of lifesavers and said, \"Children, I'd like you to close your eyes and taste these.\"
+The kids easily identified the taste of cherries, lemons and mint, but when the teacher gave them honey-flavored lifesavers, all of the kids were stumped.
+I'll give you a hint,\" said the teacher. 
+\"It's something your mommy probably calls your daddy all the time.\"
+Instantly, Little Johnny coughed his onto the floor and shouted, \"Quick! 
+Spit'em out! 
+They're assholes!\"",
+"One day at the end of class, little Johnny's teacher asks the class to go home and think of a story to be concluded with the moral of that story. 
+The following day the teacher asks for the first volunteer to tell their story.
+Little Suzy raises her hand. 
+\"My dad owns a farm and every Sunday we load the chicken eggs on the truck and drive into town to sell them at the market. 
+Well, one Sunday we hit a big bump and all the eggs flew out of the basket and onto the road.\"
+When the teacher asked for the moral of the story, Suzy replied, \"Don't keep all your eggs in one basket.\"
+Little Lucy went next. 
+\"My dad owns a farm too. 
+Every weekend we take the chicken eggs and put them in the incubator. 
+Last weekend only eight of the 12 eggs hatched.\";
+Again, the teacher asked for the moral of the story.
+Lucy replied, \"Don't count your chickens before they hatch.\"
+Next up was little Johnny. 
+\"My uncle Ted fought in the Vietnam war, and his plane was shot down over enemy territory. 
+He jumped out before it crashed but could only take a case of beer, a machine gun and a machete. 
+On the way down, he drank the case of beer. 
+Then he landed right in the middle of 100 Vietnamese soldiers. 
+He shot 70 with his machine gun, but then he ran out of bullets! 
+So he pulled out his machete and killed 20 more. 
+Then the blade on his machete broke, so he killed the last ten with his bare hands.\"
+The teacher looked a little shocked. 
+After clearing her throat, she asked what possible moral there could be to this story.
+\"Well,\" Johnny replied, \"Don't fuck with Uncle Ted when he's been drinking.\"",
+"One day, Little Johnny saw his grandpa smoking his cigarettes. Little Johnny asked, \"Grandpa, can I smoke some of your cigarettes?\" His grandpa replied, \"Can your penis reach your asshole?\" \"No\", said Little Johnny. His grandpa replied, \"Then you're not old enough.\"
+
+The next day, Little Johnny saw his grandpa drinking beer. He asked, \"Grandpa, can I drink some of your beer?\" His grandpa replied, \"Can your penis reach your asshole?\" \"No\" said Little Johhny. \"Then you're not old enough.\" his grandpa replied.
+
+The next day, Little Johnny was eating cookies. His grandpa asked, \"Can I have some of your cookies?\" Little Johnny replied, \"Can your penis reach your asshole?\" His grandpa replied, \"It most certainly can!\" Little Johnny replied, \"Then go fuck yourself.",
+"A teacher asks her class, \"What do you want to be when you grow up?\"
+Little Johnny says \"I wanna be a billionaire, going to the most expensive clubs, take the best bitch with me, give her a Ferrari worth over a million bucks, an apartment in Hawaii, a mansion in Paris, a jet to travel through Europe, an Infinite Visa Card and to make love to her three times a day\".
+The teacher, shocked, and not knowing what to do with the bad behavior of the child, decides not to give importance to what he said and then continues the lesson.
+\"And you, Susie? \" the teacher asks.
+Susie says \"I wanna be Johnny's bitch.\"",
+"Finding one of her students making faces at others on the playground, Ms. Smith stopped to gently reprimand the child.
+Smiling sweetly, the Sunday school teacher said, \"Johnny, when I was a little girl, I was told if that I made ugly faces, it would freeze and I would stay like that.\" 
+Little Johnny looked up and replied, \"Well, Ms Smith, you can't say you weren't warned.\"",
+"Little Johnny comes home from sunday school with a black eye. 
+His father sees it and says, \"Johnny, how many times do I have to tell you not to fight with the other boys?\"     \"But Dad, it wasn't my fault. 
+We were all in church saying our prayers. We all stood up and my teacher in front of me had her dress in the crack of her butt. 
+I reached over and pulled it out. 
+That's when she hit me!\"     
+\"Johnny,\" the father said. 
+\"You don't do those kind of things to women.\"     
+Sure enough, the very next sunday Johnny came home with the other eye black and blue. 
+Johnny's father said, \"Johnny, I thought we had a talk!\"     
+\"But Dad,\" Johnny said, \"It wasn't my fault. 
+There we were in church saying our prayers. 
+We all stood up and my teacher in front of us had her dress in the crack of her butt. 
+Then Louie who was sitting next to me saw it and he reached over and pulled it out. 
+Now I know she doesn't like this, so I pushed it back in!\""
+);
+
+// Select a random joke from the array.
+$joke = array_rand($jokeList);
+?>
